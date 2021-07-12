88548e36d8ea2dc4b46d5deab9a03f8a6c61cddf
Monica-Araujo <monicaaraujo974@gmail.com>
2021-07-12 17:32:29 -0300

    fazendo o header

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -13,6 +13,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     background-color: white;
     color: black;
+    height: 5000px;
   }
 
   button {
@@ -24,24 +25,49 @@ const GlobalStyle = createGlobalStyle`
 
 
 const Header = styled.header`
-  background-color: #17034d;
+  background-color: rgba(25, 0, 51, 0.9);
   height: 50px;
   display: flex;
   align-items: center;
   padding-left: 15px;
+  position: fixed;
+  width: 100vw;
 `
 
 const H1 = styled.h1`
   color: yellow;
 `
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 100px 100px 800px 100px 100px;
+`
 
+const LogoContainer = styled.div`
+  grid-column-start: 2;
+`
+
+const ButtonContainer = styled.div`
+  grid-column-start: 5;
+`
+const Button = styled.button`
+  background-color: purple;
+  color: white;
+  border: none;
+  border-radius: 15px;
+`
 function App() {
   return (
     <div>
       <GlobalStyle/>
         <Header>
-          <H1>Pokedex</H1>
-          <button>Pokedex</button>
+          <HeaderContainer>
+            <LogoContainer>
+              <H1>Pokedex</H1>
+            </LogoContainer>
+            <ButtonContainer>
+              <Button>Pokedex</Button>
+            </ButtonContainer>
+          </HeaderContainer>
         </Header>
         <Router />
     </div>
