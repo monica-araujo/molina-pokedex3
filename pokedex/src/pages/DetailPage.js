11228a1eb8fc1226ab685d0356b494e64e629528
felipe <felipesoutomdt@gmail.com>
2021-07-13 16:45:48 -0300

    finalizando esqueleto

--- pokedex/src/pages/DetailPage.js
+++ pokedex/src/pages/DetailPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import pokemon from '../img/pokemon.png'
 
+
 const DivContainer = styled.div`
     display: flex;
     align-items: center;
