a1a6f3281d2a34fa5772ff3183ce062c90b3fc78
felipe <felipesoutomdt@gmail.com>
2021-07-13 16:37:15 -0300

    finalizando esqueleto detalhes

--- pokedex/src/pages/DetailPage.js
+++ pokedex/src/pages/DetailPage.js
@@ -1,11 +1,95 @@
 import React from 'react'
 import styled from 'styled-components'
 import axios from 'axios' 
+import pokemon from '../img/pokemon.png'
+
+const DivContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 60px;
+    justify-content: center;
+`
+const DivCard = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 15px;
+    align-items: center;
+    justify-content: center;
+`
+
+const CardPokemon = styled.div`
+    background-color:  rgba(25, 0, 51, 0.9);
+    color: white;
+    border-radius: 15px;
+    display: grid;
+    align-items: center;
+    padding: 20px;
+    grid-template-columns: 1fr 1fr 1fr;
+`
+
+const DivImages = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #6B0091;
+    border-radius: 15px;
+    margin: 15px;
+    padding:15px;
+`
+
+
+const DescriptionP = styled.p`
+    padding: 15px;
+`
+
+
+
+const Title =  styled.h2`
+    color: #f9dd00;
+`
+
+const CardDescription = styled.div`
+    background-color: #6B0091;
+    border-radius: 15px;
+    margin: 15px;
+    padding:15px;
+`
 
 export const DetailPage = () => {
     return (
         <div>
-            
+            <DivContainer>
+                <h1>Blastoise</h1>
+            </DivContainer>
+            <DivCard>
+            <CardPokemon>
+                <DivImages>
+                    <img src={pokemon} alt="" />
+                    <img src={pokemon} alt="" />
+                </DivImages>
+                <CardDescription>
+                    <Title>Status</Title>
+                    <DescriptionP>hp: 60</DescriptionP>
+                    <DescriptionP>attack: 62</DescriptionP>
+                    <DescriptionP>defense: 63</DescriptionP>
+                    <DescriptionP>special-attack: 80 </DescriptionP>
+                    <DescriptionP>special-defense: 80 </DescriptionP>
+                    <DescriptionP>speed: 60</DescriptionP>
+                </CardDescription>
+                <CardDescription>
+                    <Title>Tipo</Title>
+                    <DescriptionP>grass</DescriptionP>
+                    <DescriptionP>poison</DescriptionP>
+                    <Title>Principais ataques</Title>
+                    <DescriptionP>swords-dance</DescriptionP>
+                    <DescriptionP>cut</DescriptionP>
+                    <DescriptionP>bind</DescriptionP>
+                    <DescriptionP>vine-whip </DescriptionP>
+                    <DescriptionP>headbutt </DescriptionP>
+                </CardDescription>
+            </CardPokemon>
+           
+        </DivCard>
         </div>
+        
     )
 }
