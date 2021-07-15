754c21d6d9efa6e8e09641624fac453dbfc4381a
felipe <felipesoutomdt@gmail.com>
2021-07-15 18:21:31 -0300

    renderizar pokemons

---
+++ pokedex/src/Container/PokemonCard/PokemonCard.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Pokemon} from '../../Container/Pokemon/Pokemon'
+
+
+
+
+
+const DivCard = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 15px;
+    align-items: center;
+    justify-content: center;
+`
+
+
+
+export const PokemonCard = ({pokemons}) => {
+    
+
+    
+    
+    return (
+        <div>  
+            <DivCard>
+             {
+                    pokemons?(
+                        pokemons.map((val, idx) => {
+                            return(
+                               <Pokemon key={idx} thisPokemon={val} />
+                            )
+                        }
+                            
+                        )
+                    
+                    ):(<div>Carregando</div>)
+                    
+
+           /*  
+             */
+                }
+            </DivCard>
+        </div>
+
+    )
+}
